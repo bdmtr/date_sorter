bdf3bedf31db1e3402e7365e2511fcf7b1777dbc
bdmtr <bidenko.dmtr@gmail.com>
2023-10-10 14:50:59 +0300

    add solution

---
+++ DateSorter.java
@@ -0,0 +1,47 @@
+package sample;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Marking will be based upon producing a readable, well engineered solution rather than factors
+ * such as speed of processing or other performance-based optimizations, which are less
+ * important.
+ * <p>
+ * <p>
+ * Implement in single class.
+ */
+public class DateSorter {
+    /**
+     * The implementation of this method should sort dates.
+     * The output should be in the following order:
+     * Dates with an 'r' in the month,
+     * sorted ascending (first to last),
+     * then dates without an 'r' in the month,
+     * sorted descending (last to first).
+     * For example, October dates would come before May dates,
+     * because October has an 'r' in it.
+     * thus: (2005-07-01, 2005-01-02, 2005-01-01, 2005-05-03)
+     * would sort to
+     * (2005-01-01, 2005-01-02, 2005-07-01, 2005-05-03)
+     *
+     * @param unsortedDates - an unsorted list of dates
+     * @return the collection of dates now sorted as per the spec
+     */
+    public Collection<LocalDate> sortDates(List<LocalDate> unsortedDates) {
+        // your solution here
+        Stream<LocalDate> withRInMonth = unsortedDates.stream().filter(this::hasR).sorted();
+        Stream<LocalDate> withoutRInMonth = unsortedDates.stream().filter(date -> !hasR(date)).sorted(Collections.reverseOrder());
+
+        return Stream.concat(withRInMonth, withoutRInMonth).collect(Collectors.toList());
+    }
+
+    private boolean hasR(LocalDate date) {
+        String r = date.getMonth().toString().toLowerCase();
+        return r.contains("r");
+    }
+}
