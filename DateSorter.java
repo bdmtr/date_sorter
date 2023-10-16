8632e41f7346e364dccc1acfebcc2b53a2fb29c6
bdmtr <bidenko.dmtr@gmail.com>
2023-10-16 18:02:38 +0300

    change conditions

--- DateSorter.java
+++ DateSorter.java
@@ -11,9 +11,8 @@ import java.util.stream.Stream;
  * Marking will be based upon producing a readable, well engineered solution rather than factors
  * such as speed of processing or other performance-based optimizations, which are less
  * important.
- * <p>
- * <p>
- * Implement in single class.
+ *
+ * Implement in single class. Don't chane constructor and signature method.
  */
 public class DateSorter {
     /**
@@ -25,9 +24,9 @@ public class DateSorter {
      * sorted descending (last to first).
      * For example, October dates would come before May dates,
      * because October has an 'r' in it.
-     * thus: (2005-07-01, 2005-01-02, 2005-01-01, 2005-05-03)
+     * thus: (2004-07-01, 2005-01-02, 2007-01-01, 2032-05-03)
      * would sort to
-     * (2005-01-01, 2005-01-02, 2005-07-01, 2005-05-03)
+     * (2005-01-02, 2007-01-01, 2032-05-03, 2004-07-01)
      *
      * @param unsortedDates - an unsorted list of dates
      * @return the collection of dates now sorted as per the spec
